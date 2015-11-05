5b1907fb70c93d443a85efe5c0dddd9d791aa326
AndraWP <andrawahyupurnomo17@gmail.com>
2015-11-05 12:13:41 +0700

    Create rest-api.php

---
+++ rest-api.php
@@ -0,0 +1,28 @@
+<?php
+
+# Name/NIM	: Jenika Sutojo/18213003 - Andra Wahyu Purnomo/18213004
+# Day, Date	: Thursday, 11 November 2015
+# File		: rest-api.php
+
+	function get_info_by_id($No_ID){
+		$info = array();
+		mysql_connect('localhost','root','');
+		mysql_select_db('hotel');
+		$result = mysql_query("SELECT No_ID, Nama FROM pengunjung WHERE No_ID = '$No_ID'");
+		$info = mysql_fetch_array($result,MYSQL_ASSOC);
+		return $info;
+	}
+	
+	if (isset($_GET["action"])){
+		switch ($_GET["action"]){
+			case "get_info";
+				if (isset($_GET["No_ID"]))
+					$value = get_info_by_id($_GET["No_ID"]);
+				else
+					$value = "ERROR";
+				break;	
+		}
+	}
+	
+	exit(json_encode($value));
+?>
