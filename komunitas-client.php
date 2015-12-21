8c541103fb06d4af81c10c8ee75040a56c8b6a6a
AndraWP <andrawahyupurnomo17@gmail.com>
2015-12-21 15:30:50 +0700

    Create komunitas-client.php

---
+++ komunitas-client.php
@@ -0,0 +1,170 @@
+<?php
+
+# Name/NIM	: Jenika Sutojo/18213003 - Andra Wahyu Purnomo/18213004
+# File		: komunitas-client.php
+
+session_start();
+$_SESSION = array();
+
+$captcha = file_get_contents('http://localhost/komunitas/captcha_service.php?action=get_captcha');
+$captcha = json_decode($captcha,true);
+
+$_SESSION['captcha'] = $captcha;
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>Tambah Komunitas | KM-ITB </title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link href="css/association.css" rel="stylesheet">
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/half-slider.css" rel="stylesheet">
+	</head>
+	<body>
+		<!-- Navigation -->
+		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<div class="title">
+						<a class="navbar-brand" href="#">KM-ITB</a>
+					</div>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				   </div>
+				<!-- /.navbar-collapse -->
+			</div>
+			<!-- /.container -->
+		</nav>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="menu-bar">
+						<div class= "logo">
+							<img id="logo-img" src="assets/logo.gif"></img>
+						</div>
+						<div class= "menu">
+								<ul class="nav nav-pills">
+									<li><a href="http://localhost/home/index.php">Home</a></li>
+									<li><a href="http://localhost/event/event-depan.php?action=get_all">Event</a></li>
+									<li><a href="http://localhost/komunitas/od.html">Open Data</a></li>
+									<li><a href="http://localhost/mahasiswa/datakader.html">Kader</a></li>
+									
+									<!-- authentication service -->
+									<li><a href="/authentication/logout.php?logout=true">Logout</a></li>
+								</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="satu">
+		
+		<div class="borderform">
+		<div class="judulatas">
+			<h1 class="judulh1">Komuntias Management</h1>
+			<h2 class="judulh2">Form untuk Memasukkan Data Komunitas</h2>
+		</div>
+	<div class="formevent">	
+	<form method="POST" action="http://localhost/komunitas/event-api.php" enctype="multipart/form-data">
+	  <label class="labeljudul">Nama Lembaga</label><br>
+	  <input type="text" name="namalembaga" placeholder="Serat Pena" required>
+	  <br>
+	  <label class="labeljudul">Deskripsi</label><br>
+	  <textarea name="deskripsikomunitas" placeholder="Deskripsi" required></textarea>
+	  <br>
+	  <label class="labeljudul">Kategori</label><br>
+	  <input type="text" name="kategori" placeholder="2" required>
+	  <br>
+	  <label class="labeljudul">Alamat</label><br>
+	  <textarea name="alamatkomunitas" placeholder="Komplek Bumi Mas Kencana II No.27, Cicaheum " required></textarea>
+	  <br>
+	  <label class="labeljudul">Latitude</label><br>
+	  <input type="text" name="lat" placeholder="-6.9046">
+	  <br>
+	  <label class="labeljudul">Langitude</label><br>
+	  <input type="text" name="lng" placeholder="107.66306">
+	  <br>
+	  <label class="labeljudul">Contact Person</label><br>
+	  <input type="text" name="cp" placeholder="Andra" required>
+	  <br>
+	  <label class="labeljudul">Posisi</label><br>
+	  <input type="text" name="posisi" placeholder="Humas">
+	  <br>
+	  <label class="labeljudul">Nomor telepon</label><br>
+	  <input type="text" name="nomortelepon" placeholder="08995179945" required>
+	  <br>
+	  <label class="labeljudul">Website</label><br>
+	  <input type="text" name="website" placeholder="http://seratpena.com">
+	  <br>
+	  <label class="labeljudul">Media Sosial</label><br>
+	  <input type="text" name="medsos" placeholder="t : @seratpena e : seratpena@gmail.com">
+	  <br>
+	  <label class="labeljudul">Link Gambar</label><br>
+	  <input type="text" name="linkgambar" placeholder="http://maps.google.com/mapfiles/ms/icons/red-dot.png">
+	  <br>
+	  
+	  <p>
+		Please fill the CAPTCHA below:
+      </p>
+
+      <p>
+        <?php
+        echo '<img src="data:image/jpeg;base64,'.$_SESSION['captcha']['image_src'].'"/>';
+
+        ?>
+      </p>
+	                   
+		<input type="text" name="captcha_input" />
+	  
+	  <input type="submit" value="Submit" name="submitKomunitas">
+	</form> 
+	</div>
+	</div>
+	</div>
+	
+	<!-- Page Content -->
+    <div class="container">
+	<div class="center">
+		<div class="social-media">
+			<a href='https://www.facebook.com/ITB.KM'><img id="icon-fb" src="assets/home/icon-fb.png"></img></a>
+			<a href='http://mail.google.com'><img id="icon-mail" src="assets/home/icon-mail.png"></img></a>
+			<a href='https://www.instagram.com/km_itb/'><img id="icon-ig" src="assets/home/icon-ig.png"></img></a>	
+		</div>
+	</div>
+	</div>
+	
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="copyright">
+						<p>Sistem dan Teknologi Informasi 2013</p>
+					</div>
+            </div>
+            <!-- /.row -->
+        </footer>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+	</body>
+</html>
